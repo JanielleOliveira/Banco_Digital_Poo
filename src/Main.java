e853fed91c307581aab6d727a16ae19c9882b044
Janielle Oliveira <janielleoliver@gmail.com>
2024-06-26 17:28:15 -0300

    implementando a Classe 'Main' para Testar o Projeto
    Signed-off-by: Janielle Oliveira <janielleoliver@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,32 @@
 import banco.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Marry", "000.000.123-00");
+        Cliente cliente2 = new Cliente("Martin", "000.321.000-11");
 
+        IConta contaCorrente = new ContaCorrente(cliente1);
+        IConta contaPoupanca = new ContaPoupanca(cliente2);
+
+        Banco banco = new Banco();
+        banco.adicionarConta(contaCorrente);
+        banco.adicionarConta(contaPoupanca);
+
+
+        contaCorrente.depositar(1000);
+        contaPoupanca.depositar(250);
+
+        contaCorrente.imprimirExtrato();
+        contaPoupanca.imprimirExtrato();
+
+        System.out.println("=== TRANSFERÊNCIAS ===");
+        contaCorrente.transferir(500, contaPoupanca);
+
+        System.out.println("Saldo Conta Corrente após tranferência: " + contaCorrente.getSaldo());
+        System.out.println("Saldo Conta Poupança após tranferência: " + contaPoupanca.getSaldo());
+
+
+
+
+    }
 }
