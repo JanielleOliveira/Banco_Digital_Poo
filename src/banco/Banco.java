c10669ab1972b645327a93e7f40980d581aa23df
Janielle Oliveira <janielleoliver@gmail.com>
2024-06-26 23:55:19 -0300

    Adicionando os métodos 'aplicarTaxaManuntencao', 'aplicarTaxaRendimento' em 'Banco' para simular a passagem de um mês
    Signed-off-by: Janielle Oliveira <janielleoliver@gmail.com>

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -3,8 +3,7 @@ package banco;
 import java.util.ArrayList;
 import java.util.List;
 
-//Gerencia as contas, permitindo adicionar e buscar contas.
-
+//Gerencia as contas e aplica as taxas e rendimentos.
 public class Banco {
     private final List<IConta> contas;
 
@@ -16,13 +15,31 @@ public class Banco {
         contas.add(conta);
     }
 
-    public IConta buscarConta(int numeroConta) {
+    public void aplicarTaxaManutencao() {
         for (IConta conta : contas) {
-            if (conta.getNumeroConta() == numeroConta) {
-                return conta;
+            if (conta instanceof ContaCorrente) {
+                ((ContaCorrente) conta).aplicarTaxaManutencao();
             }
         }
-        return null;
+    }
+
+    public void aplicarRendimentos() {
+        for (IConta conta : contas) {
+            if (conta instanceof ContaPoupanca) {
+                ((ContaPoupanca) conta).aplicarTaxaRendimento();
+            }
+        }
+    }
+
+    public void simularMeses(int numeroMeses) {
+        for (int i = 1; i <= numeroMeses; i++) {
+            aplicarTaxaManutencao();
+            aplicarRendimentos();
+            System.out.println("Após " + i + " mês(es): ");
+            for (IConta conta : contas){
+                System.out.println("Saldo da conta " + conta.getNumeroConta() + ": " + conta.getSaldo());
+            }
+        }
     }
 
 }
