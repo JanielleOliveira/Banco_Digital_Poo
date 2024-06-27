c10f99f61a14ce5b0d5ee34742b45c25a39dd948
Janielle Oliveira <janielleoliver@gmail.com>
2024-06-27 00:26:58 -0300

    Atualizando para utilizar o método 'simularMeses' da classe 'Banco'
    Signed-off-by: Janielle Oliveira <janielleoliver@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Cliente cliente1 = new Cliente("Marry", "000.000.123-00");
         Cliente cliente2 = new Cliente("Martin", "000.321.000-11");
 
-        IConta contaCorrente = new ContaCorrente(cliente1);
-        IConta contaPoupanca = new ContaPoupanca(cliente2);
+        IConta contaCorrente = new ContaCorrente(cliente1, 10.50); // Manutenção R$10,50 ao Mês
+        IConta contaPoupanca = new ContaPoupanca(cliente2, 0.50);// Rendimento de 0.50% ao mês
 
         Banco banco = new Banco();
         banco.adicionarConta(contaCorrente);
@@ -21,12 +21,15 @@ public class Main {
         contaCorrente.imprimirExtrato();
         contaPoupanca.imprimirExtrato();
 
+        // Simular a passagem de meses
+        System.out.println("=== APLICANDO TAXA DE MANUTENÇÃO E RENDIMENTO ===");
+        banco.simularMeses(3);
+
         System.out.println("=== TRANSFERÊNCIAS ===");
         contaCorrente.transferir(500, contaPoupanca);
 
-        System.out.println("Saldo Conta Corrente após tranferência: " + contaCorrente.getSaldo());
-        System.out.println("Saldo Conta Poupança após tranferência: " + contaPoupanca.getSaldo());
-
+        System.out.println("Saldo Conta " + contaCorrente.getNumeroConta()+ " após transferência: " + contaCorrente.getSaldo());
+        System.out.println("Saldo Conta  "+ contaPoupanca.getNumeroConta() + " após transferência: " + contaPoupanca.getSaldo());
 
 
 
